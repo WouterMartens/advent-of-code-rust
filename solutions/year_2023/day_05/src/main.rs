0074e2dd7cd633f930bc6dc9930f3481db6600df
Wouter Martens <WouterMartens94@gmail.com>
2023-12-05 21:52:23 +0100

    Add year 2023 day 5

--- solutions/year_2023/day_05/src/main.rs
+++ solutions/year_2023/day_05/src/main.rs
@@ -2,51 +2,141 @@
 #![allow(dead_code)]
 
 extern crate test;
+
 use utilities::read_input;
+use rayon::iter::ParallelIterator;
+use rayon::prelude::*;
+use indicatif::ProgressBar;
 
 const PATH: &str = "input.txt";
 
+struct Range {
+    destination: u64,
+    source: u64,
+    length: u64,
+}
+
+struct Map {
+    ranges: Vec<Range>,
+}
+
+struct Almanac {
+    seeds: Vec<u64>,
+    maps: Vec<Map>,
+}
+
+impl Almanac {
+    fn new(input: &str) -> Self {
+        let mut seeds: Vec<u64> = Vec::new();
+        let mut maps: Vec<Map> = Vec::new();
+        let mut seen_first_colon = false;
+        let mut lines = input.lines();
+    
+        while let Some(line) = lines.next() {
+            if let Some(_) = line.find(':') {
+                if !seen_first_colon {
+                    seeds = line
+                            .split_once(": ")
+                            .unwrap().1
+                            .split_ascii_whitespace()
+                            .filter_map(|s| s.parse::<u64>().ok())
+                            .collect::<Vec<_>>();
+                    seen_first_colon = true;
+                } else {
+                    process_map_lines(&mut lines, &mut maps);
+                }
+            } 
+        }
+    
+        Self { seeds, maps }
+    }
+
+    fn traverse_seed(&self, seed: u64) -> u64 {
+        let mut value = seed;
+        
+        for map in &self.maps {
+            for range in &map.ranges {
+                let in_range = value >= range.source && value <= range.source + range.length;
+                if in_range {
+                    value = value + range.destination - range.source;
+                    break;
+                }
+            }
+        }
+
+        value
+    }
+
+    fn traverse(&self) -> u64 {
+        self.seeds
+            .iter()
+            .map(|seed| self.traverse_seed(*seed))
+            .min()
+            .expect("Should have a result value")
+    }
+
+    fn traverse_range(&self) -> u64 {
+        let total_iterations: usize = self.seeds.chunks(2).map(|chunk| chunk[1] as usize).sum();
+        let progress = ProgressBar::new(total_iterations as u64);
+
+        let result = self
+            .seeds
+            .chunks(2)
+            .par_bridge()
+            .flat_map(|chunk| {
+                chunk[0]..(chunk[0] + chunk[1])
+            })
+            .map(|seed| {
+                progress.inc(1);
+                self.traverse_seed(seed)
+            })
+            .min()
+            .expect("Should have a minumum result value");
+
+        progress.finish();
+        result
+    }
+}
+
 fn main() {
     let input = read_input(PATH);
-    println!("Part 1 answer: {}", part_1(&input));
-    println!("Part 2 answer: {}", part_2(&input));
+    // let input = TEST_INPUT;
+    // println!("Part 1 answer: {:?}", part_1(&input));
+    println!("Part 2 answer: {:?}", part_2(&input));
 }
 
-fn part_1(input: &str) -> u32 {
-    input.lines();
-    0
+fn process_map_lines(lines: &mut dyn Iterator<Item = &str>, maps: &mut Vec<Map>) {
+    let mut ranges: Vec<Range> = Vec::new();
+
+    while let Some(next_line) = lines.next() {
+        if next_line.is_empty() {
+            break;
+        }
+
+        let mut values = next_line
+            .split_ascii_whitespace()
+            .filter_map(|s| s.parse::<u64>().ok());
+
+        if let (Some(destination), Some(source), Some(length)) = 
+            (values.next(), values.next(), values.next())
+        {
+            ranges.push( Range {
+                destination,
+                source,
+                length,
+            });
+        }
+    }
+
+    maps.push(Map { ranges } );
 }
 
-fn part_2(_input: &str) -> u32 {
-    0
+fn part_1(input: &str) -> u64 {
+    let almanac = Almanac::new(&input);
+    almanac.traverse()
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
-    use test::Bencher;
-
-    #[test]
-    fn test_part_1() {
-        assert_eq!(part_1(""), 0);
-        assert_eq!(part_1(""), 0);
-    }
-
-    #[test]
-    fn test_part_2() {
-        assert_eq!(part_2(""), 0);
-        assert_eq!(part_2(""), 0);
-    }
-
-    #[bench]
-    fn bench_part_1(b: &mut Bencher) {
-        let input = read_input(PATH);
-        b.iter(|| part_1(&input));
-    }
-
-    #[bench]
-    fn bench_part_2(b: &mut Bencher) {
-        let input = read_input(PATH);
-        b.iter(|| part_2(&input));
-    }
+fn part_2(input: &str) -> u64 {
+    let almanac = Almanac::new(&input);
+    almanac.traverse_range()
 }
